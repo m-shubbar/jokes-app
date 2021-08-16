0f424f9deb4a1e96f5d8ef58311927de34597573
Mustafa <codingbox@outlook.com>
2021-08-16 01:53:22 -0400

    init

---
+++ src/test/java/ca/shubbar/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.shubbar.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
