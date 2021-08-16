e820ca1b911512e9b6e0712b58ec0a2f429396b9
Mustafa <codingbox@outlook.com>
2021-08-16 02:33:53 -0400

    Templeate to show random joke has been created, Closes #2, Closes #3, Closes #4.

--- src/main/java/ca/shubbar/jokesapp/controllers/JokeController.java
+++ src/main/java/ca/shubbar/jokesapp/controllers/JokeController.java
@@ -21,7 +21,7 @@ public class JokeController {
 
     @RequestMapping({"/", ""})
     public String showJoke(Model model) {
-        model.addAttribute("Joke", jokeService.getJoke());
+        model.addAttribute("joke", jokeService.getJoke());
         return "index";
     }
 }
